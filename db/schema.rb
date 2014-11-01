183b0e92a98281a4d52f9f632ac181bb1b40bdb3
Ben Pritchard <bpritchard@westfield.com>
2014-11-01 16:20:32 +1100

    added project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101051320) do
+ActiveRecord::Schema.define(version: 20141101052009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,5 +24,11 @@ ActiveRecord::Schema.define(version: 20141101051320) do
   end
 
   add_index "people", ["email"], name: "index_people_on_email", unique: true, using: :btree
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
